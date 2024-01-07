f0a8a2151f84977be51e4dd44409965eb3667179
Thaw Zin Tun <thawzintun98@gmail.com>
2024-01-08 01:31:53 +0630

    edit

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ if (!process.env.DISABLE_XORIGIN) {
     });
 }
 
-app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/views/index.html");
