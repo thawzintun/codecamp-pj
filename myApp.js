66306632ce773063f382df613f2572abce57dea8
Thaw Zin Tun <thawzintun98@gmail.com>
2024-01-08 12:03:33 +0630

    changes

--- myApp.js
+++ myApp.js
@@ -1,40 +1,32 @@
-let express = require('express');
+let express = require("express");
 let app = express();
-console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/json", function (req, res, next) {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+});
 
+app.get(
+    "/now",
+    (req, res, next) => {
+        req.time = new Date().toString();
+        next();
+    },
+    (req, res) => {
+        res.json({ time: req.time });
+    }
+);
+app.get("/json", function (req, res) {
+    var helloMsg = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        helloMsg = "HELLO JSON";
+    }
+    res.json({ message: helloMsg });
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
--- server.js
+++ server.js
@@ -31,34 +31,6 @@ if (!process.env.DISABLE_XORIGIN) {
     });
 }
 
-app.use("/public", express.static(__dirname + "/public"));
-app.use("/json", function (req, res, next) {
-    console.log(`${req.method} ${req.path} - ${req.ip}`);
-    next();
-});
-
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/views/index.html");
-});
-
-app.get(
-    "/now",
-    (req, res, next) => {
-        req.time = new Date().toString();
-        next();
-    },
-    (req, res) => {
-        res.json({ time: req.time });
-    }
-);
-app.get("/json", function (req, res) {
-    var helloMsg = "Hello json";
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        helloMsg = "HELLO JSON";
-    }
-    res.json({ message: helloMsg });
-});
-
 const port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, () => {
     bGround.log(`Node is listening on port ${port}...`);
